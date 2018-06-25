c9fcd4c2eca8d9f545ced8e854d544af7fa4df9c
rockts <rockts@sina.com>
2018-06-25 21:53:58 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <notes></notes>
+  </div>
+</template>
+
+<script>
+import Notes from './components/Notes'
+import 'semantic-ui-css/semantic.min.css'
+
+export default {
+  name: 'App',
+  components: {
+    Notes
+  }
+}
+</script>
+
+<style>
+.notes {
+  margin-top: 32px;
+}
+
+.notes .ui textarea {
+  border: none;
+  font-size: 16px;
+  padding: 0;
+  margin: 32px 0;
+}
+
+.ui.items > .item > .content > .header {
+  display: block;
+  cursor: pointer;
+}
+
+.ui.items > .item > .meta {
+  width: 88px;
+  color: rgba(0, 0, 0, 0.4);
+  margin-top: 0;
+  margin-bottom: 16px;
+}
+
+.trash.icon {
+  cursor: pointer;
+}
+</style>
---
+++ src/components/Note.vue
@@ -0,0 +1,55 @@
+<template lang="html">
+  <div class="item">
+    <div class="meta">
+      {{ updated(entity) }}
+    </div>
+    <div class="content">
+      <div class="header" v-on:click="open = !open">
+        {{ header(entity) || '新建笔记' }}
+      </div>
+      <div class="extra">
+      <editor v-bind:entity="entity" v-if="open">
+      </editor>
+      {{ words(entity) }} 字
+      <i class="right floated trash alternate outline icon"
+        v-on:click="destroy(entity)"
+        v-if="open">
+      </i>
+    </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import Editor from './Editor'
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters([
+      'updated',
+      'words',
+      'header'
+    ])
+  },
+  data () {
+    return {
+      open: false
+    }
+  },
+  props: [
+    'entity'
+  ],
+  methods: {
+    ...mapActions([
+      'destroy'
+    ])
+  },
+  components: {
+    Editor
+  }
+}
+</script>
+
+<style lang="css">
+</style>
